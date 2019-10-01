05ff65d521d9f68852fa2e0ed874f3487a8df582
Filippo Finke <37296364+filippofinke@users.noreply.github.com>
2019-10-01 17:05:15 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -9,9 +9,9 @@ ini_set("display_errors", 1);
 define('URL', 'http://127.0.0.1/');
 
 define('DB_USER', 'root');
-define('DB_PASS', 'filospinato22');
+define('DB_PASS', 'root');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'hackerlab');
+define('DB_NAME', 'db');
 define('SQLITE', null);
 
 $autoload_directories = array(
